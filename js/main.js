182c501d534abacefaa2312f97103b2a1f39e161
DotsJulia <dotsjulia12@gmail.com>
2024-12-05 14:42:13 +0500

    Add saving mode in other pages

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
 const btnDarkMode = document.querySelector(".dark-mode-btn");
 
+if(localStorage.getItem('darkMode') === 'dark') {
+    btnDarkMode.classList.add("dark-mode-btn--active");
+    document.body.classList.add("dark");
+}
+
+//Проверка темной темы на основе системных настроек
+// if(window.matchMedia && window.matchMedia("(prefers-color-sheme: dark)").matches) {
+//     btnDarkMode.classList.add("dark-mode-btn--active");
+//     document.body.classList.add("dark");
+// }
+
+
+//Включение ночного режима по копке
 btnDarkMode.onclick = function() {
     btnDarkMode.classList.toggle("dark-mode-btn--active");
-    document.body.classList.toggle("dark")
+    const isDark = document.body.classList.toggle("dark");
+
+    if(isDark) {
+        localStorage.setItem('darkMode', 'dark')
+    } else {
+        localStorage.setItem('darkMode', 'light')
+    }
 }
